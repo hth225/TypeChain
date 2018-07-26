e3473a4ce6ed32598248e65c9cb1de90e0a0d075
hth225 <hth225@naver.com>
2018-07-26 15:29:20 +0900

    First block

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
     public index:number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index:number,
         hash: string,
@@ -21,7 +26,10 @@ class Block{
 
 const genesisBlock:Block = new Block(0,"20202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+
+const getblockChain = () : Block[] => blockchain;
+const getlatestBlock = () : Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
 export{};
