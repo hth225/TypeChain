5c29e1bef066c57de19a3bdc03943cf99972da0a
hth225 <hth225@naver.com>
2018-07-26 13:11:46 +0900

    classes in blockchain

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-;
-const person = {
-    name: "jason",
-    age: 20,
-    gender: "male"
-};
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender: string;
+// };
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const gyeong = new Human("Gyeong", 21, "female");
 const SayHi = (person) => {
     return `hello ${person.name}, you are ${person.age}!, you are ${person.gender}!`;
 };
-console.log(SayHi(person));
+console.log(SayHi(gyeong));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
-};
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender: string;
+// };
+class Human {
+    public name : string;
+    public age : number;
+    public gender : string;
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
 
-const person = {
-    name: "jason",
-    age: 20,
-    gender: "male"
-};
+const gyeong = new Human("Gyeong", 21, "female");
 
 const SayHi = (person: Human): string => {
     return `hello ${person.name}, you are ${person.age}!, you are ${person.gender}!`;
 };
 
-console.log(SayHi(person));
+console.log(SayHi(gyeong));
 
 export{};
